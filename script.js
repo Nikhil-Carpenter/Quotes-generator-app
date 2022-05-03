3e3e54319ebc0afe8dec0cc3e2950d18e2e55cee
Nikhil-Carpenter <nikhilcrpntr222@gmail.com>
2022-05-03 12:38:23 +0530

    some updates

--- script.js
+++ script.js
@@ -15,15 +15,28 @@
 //         author: '― Craig Ferguson'
 //     }
 // ]
+let quotes ={
+    "― Craig Ferguson":'“I freely admit I\'m confused. I\'m a confused and troubled individual but at the same time...Its Free! ”',
+    "― Jenny Valentine":'“Even when you’d lost everything you thought there was to lose, somebody came along and gave you something for free.”',
+    "― Aaron Lauritsen":'“There is strange comfort in knowing that no matter what happens today, the Sun will rise again tomorrow.”'
+}
+
+
+function firstLoading(){
+    document.getElementById("quote").innerText = quotes["― Craig Ferguson"];
+    let authors = Object.keys(quotes);
+    document.getElementById("author").innerText = authors[0] ;
+
+}
 
 
 function generate(){
 
-    let quotes ={
-        "― Craig Ferguson":'“I freely admit I\'m confused. I\'m a confused and troubled individual but at the same time...Its Free! ”',
-        "― Jenny Valentine":'“Even when you’d lost everything you thought there was to lose, somebody came along and gave you something for free.”',
-        "― Aaron Lauritsen":'“There is strange comfort in knowing that no matter what happens today, the Sun will rise again tomorrow.”'
-    }
+    // let quotes ={
+    //     "― Craig Ferguson":'“I freely admit I\'m confused. I\'m a confused and troubled individual but at the same time...Its Free! ”',
+    //     "― Jenny Valentine":'“Even when you’d lost everything you thought there was to lose, somebody came along and gave you something for free.”',
+    //     "― Aaron Lauritsen":'“There is strange comfort in knowing that no matter what happens today, the Sun will rise again tomorrow.”'
+    // }
 
     let authors = Object.keys(quotes);
     console.log(authors)
